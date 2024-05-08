ca2be7a8770c4992144ae9722b6af7db0bb2a354
Alexander <82374540+palex08@users.noreply.github.com>
2024-05-08 14:09:14 +1000

    Update main.py

--- main.py
+++ main.py
@@ -4,5 +4,5 @@ numbers = list(range(1, 11))
 squared_numbers = [number ** 2 for number in numbers]
 print(squared_numbers)
 
-last_numb = squared_numbers.pop()
-print(last_numb)
+last_numb_squared = squared_numbers.pop()
+print(last_numb_squared)
